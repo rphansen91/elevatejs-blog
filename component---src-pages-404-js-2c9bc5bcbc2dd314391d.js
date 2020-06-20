f922fde530b8da3b84de5a0a0ba57b5c91ec653b
rphansen91 <rphansen91@gmail.com>
2020-06-20 15:26:08 -0700

    Updates

---
+++ component---src-pages-404-js-2c9bc5bcbc2dd314391d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Bl7J"),s=n("1uMg");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(s.a,{title:"404"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2c9bc5bcbc2dd314391d.js.map
