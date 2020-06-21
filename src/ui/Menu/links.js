47a340bb81d251ea3cc65cdb6f214975c2ad23ae
rphansen91 <rphansen91@gmail.com>
2020-06-21 15:05:16 -0700

    Fix link color

--- src/ui/Menu/links.js
+++ src/ui/Menu/links.js
@@ -1,47 +1,43 @@
-import React from "react";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import Typography from "@material-ui/core/Typography";
-import HomeIcon from "@material-ui/icons/Home";
-import SettingsIcon from "@material-ui/icons/Settings";
-import MoneyIcon from "@material-ui/icons/AttachMoney";
-import AddIcon from "@material-ui/icons/Add";
-import NoteIcon from "@material-ui/icons/Note";
-import StoreIcon from "@material-ui/icons/Store";
-import GQLIcon from "@material-ui/icons/NetworkCheck";
-import HelpIcon from "@material-ui/icons/Help";
-import { Link } from "gatsby";
+import React from "react"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import Typography from "@material-ui/core/Typography"
+import HomeIcon from "@material-ui/icons/Home"
+import SettingsIcon from "@material-ui/icons/Settings"
+import MoneyIcon from "@material-ui/icons/AttachMoney"
+import AddIcon from "@material-ui/icons/Add"
+import NoteIcon from "@material-ui/icons/Note"
+import StoreIcon from "@material-ui/icons/Store"
+import GQLIcon from "@material-ui/icons/NetworkCheck"
+import HelpIcon from "@material-ui/icons/Help"
+import { Link } from "gatsby"
 
-const linkStyle = color => ({
+const linkStyle = (color) => ({
   flex: "1 1 auto",
   padding: "0 16px",
-  color
-});
+  color,
+})
 
-export const MainListItems = (props => (
+export const MainListItems = (props) => (
   <List className={props.className || ""}>
-    <Link to="/" aria-label="home">
-      <ListItem button>
-        <ListItemIcon>
-          <HomeIcon />
-        </ListItemIcon>
-        <ListItemText>
-          <Typography color="textPrimary">Home</Typography>
-        </ListItemText>
-      </ListItem>
-    </Link>
-    <Link to="/blog/" aria-label="blog">
-      <ListItem button>
-        <ListItemIcon>
-          <NoteIcon />
-        </ListItemIcon>
-        <ListItemText>
-          <Typography color="textPrimary">Blog</Typography>
-        </ListItemText>
-      </ListItem>
-    </Link>
+    <ListItem button component={Link} to="/">
+      <ListItemIcon>
+        <HomeIcon />
+      </ListItemIcon>
+      <ListItemText>
+        <Typography color="textPrimary">Home</Typography>
+      </ListItemText>
+    </ListItem>
+    <ListItem button component={Link} to="/blog/">
+      <ListItemIcon>
+        <NoteIcon />
+      </ListItemIcon>
+      <ListItemText>
+        <Typography color="textPrimary">Blog</Typography>
+      </ListItemText>
+    </ListItem>
     {/* <Link to="/shop/" aria-label="shop">
       <ListItem button>
         <ListItemIcon>
@@ -53,30 +49,26 @@ export const MainListItems = (props => (
       </ListItem>
     </Link> */}
   </List>
-));
+)
 
-export const OtherListItems = (props => (
+export const OtherListItems = (props) => (
   <List className={props.className || ""}>
-    <Link to="/about/" aria-label="about">
-      <ListItem button>
-        <ListItemIcon>
-          <HelpIcon />
-        </ListItemIcon>
+    <ListItem button component={Link} to="/about/">
+      <ListItemIcon>
+        <HelpIcon />
+      </ListItemIcon>
 
-        <ListItemText>
-          <Typography color="textPrimary">About</Typography>
-        </ListItemText>
-      </ListItem>
-    </Link>
-    <Link to="/settings/" aria-label="settings">
-      <ListItem button>
-        <ListItemIcon>
-          <SettingsIcon />
-        </ListItemIcon>
-        <ListItemText>
-          <Typography color="textPrimary">Settings</Typography>
-        </ListItemText>
-      </ListItem>
-    </Link>
+      <ListItemText>
+        <Typography color="textPrimary">About</Typography>
+      </ListItemText>
+    </ListItem>
+    <ListItem button component={Link} to="/settings/">
+      <ListItemIcon>
+        <SettingsIcon />
+      </ListItemIcon>
+      <ListItemText>
+        <Typography color="textPrimary">Settings</Typography>
+      </ListItemText>
+    </ListItem>
   </List>
-));
+)
